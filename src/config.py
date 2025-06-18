4c62867b0a7f1cd19ba38f205bf4ebd3616e0e41
Sean Kelley <seangtkelley@gmail.com>
2025-06-18 10:46:37 +0100

    python project init with pip and conda

---
+++ src/config.py
@@ -0,0 +1,6 @@
+import os
+
+REPO_ROOT = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(REPO_ROOT, "data")
+FIGURES_DIR = os.path.join(REPO_ROOT, "figures")
+SRC_DIR = os.path.join(REPO_ROOT, "src")
