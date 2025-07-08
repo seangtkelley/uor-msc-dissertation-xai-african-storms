f52d3d9189659c954fd77acc5a1532528c8bdf6e
Sean Kelley <seangtkelley@gmail.com>
2025-07-08 10:18:48 +0100

    reorg feature cols; storm_ prefix for agg features

--- src/config.py
+++ src/config.py
@@ -56,30 +56,36 @@ COL_RENAME_MAP = {
     "Lat": "y",
     "Storm": "storm_id",
     "Area": "area",
-    "duration_hr": "total_duration",
     "u_ms": "zonal_speed",
     "v_ms": "meridional_speed",
+    "duration_hr": "storm_total_duration",
 }
 
+# TODO: Uncomment when the features are ready
 FEATURE_COL_NAMES = [
+    ### point features ###
     "eat_hours",
     "x",
     "y",
     "orography_height",
-    "anor",
-    "total_duration",
+    "anor", 
     "zonal_speed",
     "meridional_speed",
     "area",
-    "mean_land_skt",
-    "mean_dthetae_dp_900_750",
-    "mean_dthetae_dp_750_500",
-    "ushear_850",
-    "mean_prcp_400",
+    # "mean_land_skt",
+    # "mean_dthetae_dp_900_750",
+    # "mean_dthetae_dp_750_500",
+
+    ### storm aggregated features ###
+    "storm_total_duration",
+    # "mean_prcp_400",
+
+    ### external features ###
+    # "ushear_850",
     "mjo_phase",
     "mjo_amplitude",
 ]
 
-TARGET_COL_NAMES = ["total_duration", "mean_prcp_400"]
+TARGET_COL_NAMES = ["storm_total_duration", "mean_prcp_400"]
 
 DATASET_COL_NAMES = ["storm_id", "timestamp"] + FEATURE_COL_NAMES
