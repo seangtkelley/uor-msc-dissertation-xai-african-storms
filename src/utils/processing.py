2fa2b3daaab7071678d82ca7d2351273da9b5e59
Sean Kelley <seangtkelley@gmail.com>
2025-07-30 14:17:01 +0100

    move masking to parent func; don't drop to preserve coords

--- src/utils/processing.py
+++ src/utils/processing.py
@@ -298,7 +298,6 @@ def calc_spatiotemporal_mean_at_point(
     radius_km: float = 400,
     timedelta: Optional[pd.Timedelta] = None,
     invariant: bool = False,
-    mask: Optional[xr.DataArray] = None,
     variable_bounds: Optional[tuple[float, float]] = None,
 ) -> np.floating:
     """
@@ -313,17 +312,12 @@ def calc_spatiotemporal_mean_at_point(
     :param radius_km: Radius in kilometers for the spatial mean calculation.
     :param timedelta: Time delta to consider for the spatial mean calculation.
     :param invariant: If True, the variable is invariant in time (e.g., static data).
-    :param mask: Optional mask to apply to the variable values (e.g., land-sea mask).
     :param variable_bounds: Optional tuple of (lower, upper) bounds to filter the variable values.
     :return: Spatial mean of the specified variable within the radius.
     :rtype: np.floating
     """
     # convert radius to meters
     radius_m = radius_km * 1000
-
-    # if mask is provided, apply it to the dataset over the entire grid
-    if mask is not None:
-        dataset = dataset.where(mask, drop=True)
 
     # vars for dataset longitude and latitude arrays
     dataset_lons = dataset["longitude"].values
@@ -361,6 +355,7 @@ def calc_spatiotemporal_mean_at_point(
     var_over_grid = dataset.isel(
         longitude=slice(lon_start, lon_end),
         latitude=slice(lat_start, lat_end),
+        missing_dims="warn",
     )
 
     # if invariant, take the first time step
@@ -438,6 +433,10 @@ def calc_spatiotemporal_mean(
             config.DATA_DIR / "std" / f"{filename_prefix}{year}.nc"
         )
 
+        # if mask is provided, apply it to the dataset over the entire grid
+        if mask is not None:
+            dataset = dataset.where(mask)
+
         # calculate the spatial mean at each point
         processed_df.loc[group.index, new_col_name] = group.parallel_apply(  # type: ignore
             lambda row: calc_spatiotemporal_mean_at_point(
@@ -449,7 +448,6 @@ def calc_spatiotemporal_mean(
                 radius_km=radius_km,
                 timedelta=timedelta,
                 invariant=invariant,
-                mask=mask,
                 variable_bounds=variable_bounds,
             ),
             axis=1,
