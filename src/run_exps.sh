0632c038ec4b5d0e55685ef20eb7e11387de9498
Sean Kelley <seangtkelley@gmail.com>
2025-08-23 18:52:48 +0100

    fix exp order; dynamically set parallel count by number of exps; python path variable

--- src/run_exps.sh
+++ src/run_exps.sh
@@ -6,19 +6,24 @@ module load jaspy
 # get current directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# build python path
+PYTHON_PATH="$HOME/.conda/envs/uor-msc-dissertation-xai-african-storms/bin/python"
+
 # names of experiments to run separated by new lines
-EXPERIMENTS="max_intensity_era5
-direction_era5
+EXPERIMENTS="max_intensity_all
+max_intensity_era5
 direction_all
-max_intensity_all
-intensification_era5
+direction_era5
 intensification_all
+intensification_era5
 next_direction_all
 next_direction_era5
 next_distance_all
 next_distance_era5
-precipitation_era5
-precipitation_all"
+precipitation_all
+precipitation_era5"
+
+EXPERIMENT_COUNT=$(echo "$EXPERIMENTS" | wc -l)
 
 # run run_exp.py in parallel for each experiment
-echo "$EXPERIMENTS" | parallel -j 4 $HOME/.conda/envs/uor-msc-dissertation-xai-african-storms/bin/python $DIR/run_exp.py --exp_name {}
+echo "$EXPERIMENTS" | parallel -j $EXPERIMENT_COUNT $PYTHON_PATH $DIR/run_exp.py --exp_name {}
