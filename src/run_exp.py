28db7cfbfb4ef2c5e7debcbb960299b344aed527
Sean Kelley <seangtkelley@gmail.com>
2025-08-27 11:01:01 +0100

    add header to run exp script

--- src/run_exp.py
+++ src/run_exp.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+
+"""run_exp.py: Script to run experiments with different configurations"""
+
+__author__ = "Sean Kelley"
+__copyright__ = "Copyright 2025, University of Reading"
+__credits__ = ["Sean Kelley"]
+__license__ = "MIT"
+__version__ = "0.1.0"
+__maintainer__ = "Sean Kelley"
+__email__ = "s.g.t.kelley@student.reading.ac.uk"
+__status__ = "Development"
+
 import argparse
 
 import pandas as pd
