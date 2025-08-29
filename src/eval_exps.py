71ad01674d3a1e9c3b50db562a4048ca009be3e1
Sean Kelley <seangtkelley@gmail.com>
2025-08-29 09:37:17 +0100

    plot size fix with axes param; cast bool to int; TreeExplainer; new shap api style; beeswarm direct call

--- src/eval_exps.py
+++ src/eval_exps.py
@@ -11,6 +11,7 @@ __maintainer__ = "Sean Kelley"
 __email__ = "s.g.t.kelley@student.reading.ac.uk"
 __status__ = "Development"
 
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -35,7 +36,7 @@ processed_df = pd.read_csv(
 
 for exp_group_name, exp_names in config.EXPERIMENT_GROUPS.items():
 
-    fig, axs = plt.subplots(2, len(exp_names), figsize=(24, 6 * len(exp_names)))
+    fig = plt.figure(figsize=(16, 6 * len(exp_names)))
 
     # evaluate each experiment in the group
     for i, exp_name in enumerate(exp_names):
@@ -85,7 +86,8 @@ for exp_group_name, exp_names in config.EXPERIMENT_GROUPS.items():
         test_std = np.std(y_test)
 
         # plot predictions vs actual using matplotlib
-        axs[0, i].scatter(y_pred, y_test, s=10)
+        ax_pred = fig.add_subplot(2, len(exp_names), i + 1)
+        ax_pred.scatter(y_pred, y_test, s=10)
 
         # Regression line and R value using sklearn
         lr = LinearRegression()
@@ -96,37 +98,47 @@ for exp_group_name, exp_names in config.EXPERIMENT_GROUPS.items():
         )
 
         # plot regression line
-        axs[0, i].plot(
+        ax_pred.plot(
             np.unique(y_pred),
             reg_line,
             label=f"Regression line (R²={r_squared:.2f})",
             color="black",
             linestyle="--",
         )
-
-        axs[0, i].set_title(f"Model Verification for {exp_name}")
-        axs[0, i].set_xlabel("Predicted Value (K)")
-        axs[0, i].set_ylabel("Actual Value (K)")
-        axs[0, i].legend()
+        ax_pred.set_title(f"Model Verification for {exp_name}")
+        ax_pred.set_xlabel("Predicted Value (K)")
+        ax_pred.set_ylabel("Actual Value (K)")
+        ax_pred.legend()
 
         # sample X_test for faster shap value calc
         X_test_sample = X_test.sample(
             frac=0.2, random_state=config.RANDOM_STATE
         )
 
+        # cast bool to int
+        X_test_sample = X_test_sample.astype(
+            {
+                col: int
+                for col in X_test_sample.select_dtypes(include="bool").columns
+            }
+        )
+
         # get shap values for test set
-        explainer = shap.Explainer(best_model)
-        shap_values = explainer(X_test_sample)
+        explainer = shap.TreeExplainer(best_model, X_test_sample)
+        explanation = explainer(X_test_sample)
 
         # plot SHAP summary plot
-        plt.sca(axs[1, i])
-        shap.summary_plot(
-            shap_values,
-            X_test_sample,
+        ax_shap = fig.add_subplot(2, len(exp_names), len(exp_names) + i + 1)
+        shap.plots.beeswarm(
+            explanation,
             show=False,
+            plot_size=None,
+            ax=ax_shap,
+            group_remaining_features=False,
+            max_display=12,
         )
-        axs[1, i].set_title(f"SHAP Summary Plot for {exp_name}")
-        axs[1, i].tick_params(axis="y", labelsize=4)
+        ax_shap.set_title(f"SHAP Summary Plot for {exp_name}")
+        ax_shap.tick_params(axis="y", labelsize=10)
 
     plotting.save_plot(f"{exp_group_name}.png", config.EXPERIMENT_FIGURES_DIR)
 
