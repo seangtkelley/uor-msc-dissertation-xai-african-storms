54244dcc2123c949e91868e017db1c934c6a588a
Sean Kelley <seangtkelley@gmail.com>
2025-09-04 19:57:02 +0100

    plot soil water

--- src/climatology.py
+++ src/climatology.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import xarray as xr
 import config
 from utils import plotting
@@ -7,7 +8,9 @@ import numpy as np
 years = [2014, 2015, 2016, 2017, 2018, 2019]
 
 
-def load_climatology_data(filename: str) -> xr.Dataset:
+def load_climatology_data(
+    filename: str, squeeze: Optional[bool] = True
+) -> xr.Dataset:
     dataset = xr.open_dataset(
         config.DATA_DIR / "std" / f"{filename}{years[0]}.nc"
     )
@@ -16,7 +19,8 @@ def load_climatology_data(filename: str) -> xr.Dataset:
             config.DATA_DIR / "std" / f"{filename}{year}.nc"
         )
         dataset = xr.concat([dataset, next_dataset], dim="valid_time")
-    dataset = dataset.squeeze("pressure_level")
+    if squeeze:
+        dataset = dataset.squeeze("pressure_level")
     dataset = dataset.sortby("valid_time")
     dataset = dataset.mean(dim="valid_time")
     return dataset
@@ -26,6 +30,7 @@ print("Loading data...")
 dataset_uwind = load_climatology_data("uwnd_850_")
 dataset_vwind = load_climatology_data("vwnd_850_")
 dataset_shum = load_climatology_data("shum_850_")
+dataset_swvl = load_climatology_data("swvl1_d1_", squeeze=False)
 
 # Plot humidity
 print("Plotting mean humidity...")
@@ -46,6 +51,27 @@ plotting.add_gridlines(ax)
 plotting.add_borders(ax)
 ax.set_title("")
 plotting.save_plot("mean_shum_850.png", show=True)
+
+# Plot mean soil water volume
+print("Plotting mean soil water volume...")
+ax = plotting.init_map()
+dataset_swvl["swvl1"].plot.contourf(
+    ax=ax,
+    levels=10,
+    alpha=0.5,
+    cmap="Blues",
+    cbar_kwargs={
+        "label": "Mean Soil Water Volume (m³/m³)",
+        "location": "bottom",
+        "shrink": 0.8,
+        "orientation": "horizontal",
+    },
+)
+plotting.add_gridlines(ax)
+plotting.add_borders(ax)
+ax.set_title("")
+plotting.save_plot("mean_swvl1.png", show=True)
+
 
 # Plot wind
 print("Plotting mean wind speed...")
